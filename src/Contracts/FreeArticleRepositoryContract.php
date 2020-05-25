75bfc66deafa5141b007e1c97ffa0d2eb2eb019c
Nicolas Almy <nicolas.almy@nicomania.ch>
2020-05-25 08:16:58 +0200

    added config.json and Config.properties to project

--- src/Contracts/FreeArticleRepositoryContract.php
+++ src/Contracts/FreeArticleRepositoryContract.php
@@ -7,11 +7,8 @@ use FreeArticles\Models\FreeArticle;
 interface FreeArticleRepositoryContract 
 {
     public function createFreeArticle(array $data): FreeArticle;
-
     public function getFreeArticleList(): array;
-
     public function updateFreeArticle($id, array $data): FreeArticle;
-
     public function deleteFreeArticle($id): FreeArticle;
 }
 
