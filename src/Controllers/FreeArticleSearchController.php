f991a394294770adb4af5c88cb15e4e3f3652601
Nicolas Almy <nicolas.almy@nicomania.ch>
2020-05-29 09:16:44 +0200

    refactored searchFreeArticleTag

--- src/Controllers/FreeArticleSearchController.php
+++ src/Controllers/FreeArticleSearchController.php
@@ -9,31 +9,23 @@ use Plenty\Modules\Item\Variation\Contracts\VariationSearchRepositoryContract;
 
 class FreeArticleSearchController extends Controller
 {
-    // TODO The tag should be a setting later and not hardcoded 
     /**
-     * Searches the all variations which have a variationTagId with the number 126 which equals the 
-     * freeArticle tag id
      */
     public function searchFreeArticleTag() 
     {
         $repo = pluginApp(VariationSearchRepositoryContract::class);
 
-        $repo->setFilters([
-            'variationTagId' => 126
-        ]);
-
         $repo->setSearchParams([
             'variationTagId' => 126,
             'with'  => [
                 'item' => null,
-                'stock' => null,
             ] 
         ]);
     
-        $variation = $repo->search();
-        $result = $variation->getResult();
+        $result = $repo->search();
+        $variation = $result->getResult();
 
-        return $result;
+        return $variation;
     }
 }
 
